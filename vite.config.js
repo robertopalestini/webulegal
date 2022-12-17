91b345422dbb9d24673e6de85c7121b132b6c4f8
robertopalestini <robertopaletini@gmail.com>
2022-12-17 12:30:13 -0300

    fix v-if for fields in writing automatic

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,17 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vitePluginRaw from 'vite-plugin-raw'
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    // vitePluginRaw({
+    //   match: /\.svg$/,
+    //   // exclude: [new RegExp(path.resolve(__dirname, './src/assets'))]
+    // })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
